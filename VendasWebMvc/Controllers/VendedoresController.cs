72ae496b06619bb42387789ec94424853f4c9a0f
Fagner Fernandes <Fagner Fernandes@DESKTOP-ACNLJLV>
2022-08-15 19:43:35 -0300

    VendedorFormViewModel e componente select para departamento

--- VendasWebMvc/Controllers/VendedoresController.cs
+++ VendasWebMvc/Controllers/VendedoresController.cs
@@ -1,17 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
 using VendasWebMvc.Models;
 using VendasWebMvc.Services;
+using VendasWebMvc.Models.ViewModels;
 
 namespace VendasWebMvc.Controllers
 {
     public class VendedoresController : Controller
     {
         private readonly VendedorService _vendedorService;
+        private readonly DepartamentoService _departamentoService;
 
-        public VendedoresController(VendedorService vendedorService)
+        public VendedoresController(VendedorService vendedorService, DepartamentoService departamentoService)
         {
             _vendedorService = vendedorService;
+            _departamentoService = departamentoService;
         }
         public IActionResult Index()
         {
@@ -20,7 +22,9 @@ namespace VendasWebMvc.Controllers
         }
         public IActionResult Criar()
         {
-            return View();
+            var departamentos = _departamentoService.FindAll();
+            var viewModel = new VendedorFormViewModel { Departamentos = departamentos };
+            return View(viewModel);
         }   
 
         [HttpPost]
---
+++ VendasWebMvc/Services/DepartamentoService.cs
@@ -0,0 +1,21 @@
+﻿using VendasWebMvc.Data;
+using System.Collections.Generic;
+using System.Linq;
+using VendasWebMvc.Models;
+using System;
+namespace VendasWebMvc.Services
+{
+    public class DepartamentoService
+    {
+        private readonly VendasWebMvcContext _context;
+
+        public DepartamentoService(VendasWebMvcContext context)
+        {
+            _context = context;
+        }
+        public List<Departamento> FindAll()
+        {
+            return _context.Departamento.OrderBy(x => x.Nome).ToList();
+        }
+    }
+}
